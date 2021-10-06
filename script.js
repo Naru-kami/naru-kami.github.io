a9b575ef7594144dd5eb7f68699b7615cda928a8
Naru-kami <91275265+Naru-kami@users.noreply.github.com>
2021-10-06 17:16:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function wishing(wanted){
     var counter4 = 1;
 
     while (promoted < wanted){
+    while (promoted < wanted) {
         pulls += 1;
         prob5 = _rate5 + Math.min(1, Math.max(0, (counter5 - _pity5) * 10 * _rate5));
         prob4 = _rate4 + Math.min(1, Math.max(0, (counter5 - _pity4) * 10 * _rate4));
