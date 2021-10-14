c43578eaa5871fd39b8e047775aa5c17c0104c5a
Naru-kami <91275265+Naru-kami@users.noreply.github.com>
2021-10-14 17:03:37 +0200

    Polish chart (1)

---
+++ chart.js
@@ -0,0 +1,59 @@
+let myChart = document.getElementById("myChart").getContext("2d");
+
+let simulChart = new Chart(myChart, {
+    type:"line",
+    data:{
+        labels:[0,23,54,120,233,351,1049],
+        datasets:[{
+            label: "",
+            data: [0,10,25,50,75,90,99.9],
+            fill : true,
+            tension : 0.2,
+            borderColor: "rgba(23,114,203,0.8)",
+            fillColor: "rgba(151,205,187,1)"
+        }]
+    },
+    options:{
+        plugins:{
+            title:{ display: false },
+            legend:{ display: false },
+            tooltip: {
+                displayColors: false,
+                callbacks: {
+                    label: function(context) {
+                        let label = context.dataset.label;
+                        if (context.parsed.y !== null) {
+                            label += (context.parsed.y).toFixed(1) + " %";
+                        }
+                        return label;
+                    },
+                    title: function(context) {
+                        return "pulls";
+                    }
+                },
+                titleFont: { weight: "normal" },
+            }
+        },
+        scales:{
+            y: {
+                ticks: {
+                    callback: function(value, index, values) {
+                        return value + "%";
+                    }
+                },
+                max: 100,
+                beginAtZero: true
+            },
+            x: {
+                title: {
+                    display: true,
+                    text: "Number of pulls"
+                }
+            }
+        },
+        interaction: {
+            intersect: false,
+            mode: 'x'
+        }
+    }
+});
--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function wishing(wanted) {
-    var _rate5 = 0.006;
-    var _rate4 = 0.051;
-    var _pity5 = 73;
-    var _pity4 = 8
-    var promoted = 0;
-    var pulls = 0;
-    var starglitters = 0;
-    var prob5, prob4;
+    const _rate5 = 0.006;
+    const _rate4 = 0.051;
+    const _pity5 = 73;
+    const _pity4 = 8
+    const promoted = 0;
+    const pulls = 0;
+    const starglitters = 0;
+    const prob5, prob4;
 
-    var gc = 0
-    var g = false;
-    var counter5 = 1;
-    var counter4 = 1;
+    const gc = 0
+    const g = false;
+    const counter5 = 1;
+    const counter4 = 1;
 
     while (promoted < wanted) {
         pulls += 1;
         prob5 = _rate5 + Math.min(1, Math.max(0, (counter5 - _pity5) * 10 * _rate5));
         prob4 = _rate4 + Math.min(1, Math.max(0, (counter5 - _pity4) * 10 * _rate4));
 
-        var x = Math.random();
+        const x = Math.random();
 
         if (x < prob5) {
             if (x <= prob5/2 || gc == 1){
