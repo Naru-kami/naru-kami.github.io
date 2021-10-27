cd807441bda9a89377d42f0dd789275e805986c1
Naru-kami <91275265+Naru-kami@users.noreply.github.com>
2021-10-27 20:25:15 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -26,7 +26,9 @@ let simulChart = new Chart(myChart, {
                     },
                     title: function(context) {
                         let title = "";
-                        if(context[0].parsed.y <= 99.8 && context[0].parsed.y >= 0.2){
+                        if (context[0].parsed.y == 0 || context[0].parsed.y > 99.99999){
+                            title += (context[0].parsed.y).toFixed(0) + " %";
+                        }else if(context[0].parsed.y <= 99.8 && context[0].parsed.y >= 0.2){
                             title += (context[0].parsed.y).toFixed(1) + " %";
                         } else if (context[0].parsed.y > 99.98 || context[0].parsed.y < 0.02) {
                             title += (context[0].parsed.y).toFixed(3) + " %";
