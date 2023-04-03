3e54446fb131767c761c519ccb5de069ec422202
KevinHelders <91275265+Naru-kami@users.noreply.github.com>
2023-04-03 22:07:55 +0200

    try deploying

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/naru-kami.github.io/',
   build: { chunkSizeWarningLimit: 4000, },
   plugins: [react()],
 })
